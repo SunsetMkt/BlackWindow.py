b81a53f3887cfd8fd3fdf096ff4d676c073c0a29
Sunset <26019675+lwd-temp@users.noreply.github.com>
2022-12-18 16:41:13 +0800

    Better

--- BlackWindow.py
+++ BlackWindow.py
@@ -1,8 +1,14 @@
 # A fullscreen black window.
-# Close on click.
+# Close on click. Hide cursor.
 import tkinter
 root = tkinter.Tk()
+# Make the window fullscreen.
 root.attributes("-fullscreen", True)
+# Make the window black.
 root.configure(background='black')
+# Close the window on click.
 root.bind("<Button-1>", lambda event: root.destroy())
+# Hide cursor.
+root.config(cursor="none")
+# Start the window.
 root.mainloop()
